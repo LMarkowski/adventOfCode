d53170cdce9cb2c3101ec7825bbc5f1a01d04c0e
Łukasz Markowski <l.markowski95@gmail.com>
2022-12-06 09:04:38 +0100

    Shorten the loop.

--- 2022/AOC_2022_6/main.py
+++ 2022/AOC_2022_6/main.py
@@ -7,21 +7,17 @@ def read_file(filename):
 
 
 def find_marker(signal, buffer_length):
-    count = 2
-    buffer = [signal[0]]
+    buffer = list(signal[:buffer_length])
+    count = buffer_length
 
-    for i in range(1, len(signal)):
+    for i in range(buffer_length, len(signal)):
         buffer.append(signal[i])
-
-        # delete the 1st item so buffer stays the same length
-        if len(buffer) > buffer_length:
-            del buffer[0]
-
-        # only escape if the buffer reached the expected length and there are no duplicates
-        if len(buffer) == buffer_length and len(buffer) == len(set(buffer)):
-            break
-
+        del buffer[0]  # remove 1st charachter so the buffer stays the same length
         count += 1
+
+        # check for duplicates
+        if len(buffer) == len(set(buffer)):
+            break
 
     return count
 
