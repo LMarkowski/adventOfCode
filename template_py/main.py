9b2be1ba5c7cb02d6138d098fc64ca20bdfc638e
Łukasz Markowski <l.markowski95@gmail.com>
2022-12-18 16:17:03 +0100

    Update template.

--- template_py/main.py
+++ template_py/main.py
@@ -1,3 +1,8 @@
+import time
+
+debug = 1  # 1 for testing | 0 for the final run
+
+
 def read_file(filename):
     data = []
     with open(filename, "r") as file:
@@ -7,8 +12,10 @@ def read_file(filename):
 
 
 def main():
-    debug = 1  # 1 for testing | 0 for the final run
+    start_time = time.time()
     data = read_file("test-data.txt") if debug else read_file("data.txt")
+
+    print(f"\nFinished in {round(time.time() - start_time, 5)} seconds.")
 
 
 if __name__ == "__main__":
